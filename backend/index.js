e1a3c680ac8f71ce454c27aad9e6d245f12d6f2e
vuphuong1794 <phuongvu1794@gmail.com>
2024-04-18 20:29:41 +0700

    connect DB

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
-console.log("hello")
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const mongoose = require("mongoose")
+
+dotenv.config()
+
+const app = express();
+
+
+const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("connected to database");
+    } catch (err) {
+      throw err;
+    }
+  };
+  mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+  });
+  
+  mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+  });
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(8000,()=>{
+    connect();
+    console.log("server is running on port 8000")
+})
